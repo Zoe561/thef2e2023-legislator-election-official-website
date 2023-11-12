0b305ae8b2dcb26d1e3dc8b8a907cac03a1e1c78
Zoe <zoeforworks@gmail.com>
2023-11-12 21:14:58 +0800

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoadingComponent } from './pages/loading/loading.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoadingComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
