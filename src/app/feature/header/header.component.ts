b186b4a4610ee54966ebfd833b8abf7cbd210151
Zoe <zoeforworks@gmail.com>
2023-11-18 02:54:53 +0800

    fix: 路徑

--- src/app/feature/header/header.component.ts
+++ src/app/feature/header/header.component.ts
@@ -9,7 +9,7 @@ export class HeaderComponent {
   // @ViewChild('content') content?: ElementRef;
   menuVisible = false;
   menuClick = false;
-  menuList = ["小額捐款", "候選主張", "最新活動", "政策議題", "服務信箱"]
+  menuList = [{ id: '#donate', name: "小額捐款" }, { id: '#mention', name: "候選主張" }, { id: '#news', name: "最新活動" }, { id: '#issue', name: "政策議題" }, { id: '#serveEmail', name: "服務信箱" }]
   @HostListener('window:resize', ['$event'])
   onResize(event: any) {
     // 檢查視窗的新尺寸並適當地設置 menuVisible
